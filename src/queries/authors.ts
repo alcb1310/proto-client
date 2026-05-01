6a221b79ea3d680862b43078471b77eca3a4f57a
Andres Court <andres@andrescourt.com>
2026-05-01 14:30:02 -0500

    feat(authors): function to post to the golang server

--- src/queries/authors.ts
+++ src/queries/authors.ts
@@ -1,7 +1,35 @@
+import type * as pb from "#/generated/books";
+
 export async function GetAuthors() {
     try {
         const response = await fetch("http://localhost:8080/protobuf/authors", {
             method: "GET",
+        });
+
+        console.log("data fetched", response.ok);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const buffer = await response.arrayBuffer();
+
+        return buffer;
+    } catch (e) {
+        console.error("Error fetching authors:", e);
+        throw e;
+    }
+}
+
+export async function CreateAuhtor(author: pb.schema.Author) {
+    try {
+        const response = await fetch("http://localhost:8080/protobuf/authors", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/protobuf",
+            },
+            // @ts-expect-error: sending a buffer
+            body: author.serializeBinary(),
         });
 
         console.log("data fetched", response.ok);
