2a8e23d3a51fd7e6fa7719d41c746b80ae5fd022
Andres Court <andres@andrescourt.com>
2026-05-01 14:37:20 -0500

    feat(authors): added form to create an author

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import * as pb from "#/generated/books";
-import { GetAuthors } from "#/queries/authors";
+import { CreateAuhtor, GetAuthors } from "#/queries/authors";
 
 export const Route = createFileRoute("/")({
     component: Home,
@@ -23,12 +23,29 @@ function Home() {
 
     const list = authors.map((author) => <li key={author.id}>{author.name}</li>);
 
+    function onSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const data = document.getElementById("name");
+        // @ts-expect-error: value is a property of data
+        const name = data.value;
+
+        const author = new pb.schema.Author();
+        author.name = name;
+        CreateAuhtor(author);
+    }
+
     return (
         <div className="p-8">
             <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
             <p className="mt-4 text-lg">
                 Edit <code>src/routes/index.tsx</code> to get started.
             </p>
+
+            <h2 className="mt-8 text-2xl font-bold">Create Author</h2>
+            <form onSubmit={onSubmit} className="flex gap-4">
+                <input className="border" id="name" type="text" placeholder="Name" />
+                <button type="submit">Create</button>
+            </form>
 
             <h2 className="mt-8 text-2xl font-bold">Authors</h2>
             <ul>{list}</ul>
